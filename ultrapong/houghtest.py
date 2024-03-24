edef37a577b735c52929791fffa3533355da1624
Michael Fatemi <myfatemi04@gmail.com>
2024-03-23 20:31:15 -0400

    more color filter stuff

--- ultrapong/houghtest.py
+++ ultrapong/houghtest.py
@@ -7,9 +7,14 @@ import cv2
 import numpy as np
 
 DO_CAPTURE = False
+DO_PLAYBACK = True
 
-# cap = cv2.VideoCapture("video2.mp4")
-cap = cv2.VideoCapture(int(sys.argv[1]))
+if DO_PLAYBACK:
+    assert not DO_CAPTURE
+    cap = cv2.VideoCapture("video.mp4")
+else:
+    cap = cv2.VideoCapture(int(sys.argv[1]))
+    cap.set(cv2.CAP_PROP_FPS, 60)
 
 if DO_CAPTURE:
     writer = cv2.VideoWriter("video_tmp.mp4", cv2.VideoWriter_fourcc(*'mp4v'), 30, (1920, 1080))
@@ -24,6 +29,8 @@ hsv_ball = cv2.cvtColor(bgr_ball, cv2.COLOR_BGR2HSV)[0, 0]
 hsv_ball_min = np.array([10, 100, 100])
 hsv_ball_max = np.array([40, 255, 255])
 
+previous_detection = None
+
 try:
     while True:
         ret, frame = cap.read()
@@ -32,6 +39,8 @@ try:
 
         if writer is not None:
             writer.write(frame)
+
+        time.sleep(0.1)
 
         timestamps.append(time.time())
         if len(timestamps) > 1:
@@ -47,26 +56,31 @@ try:
 
         1. Color filter: We search for neon colors - colors with high saturation.
         """
-        target_r = 0.6666
-        target_g = 1 - target_r
+        target_r = 0.5555
+        target_g = 0.3555
+        target_b = 1 - target_g - target_r
+        A1 = np.array([target_b, target_g, target_r])
+        A2 = np.array([target_b, -target_r, target_g])
+        
         transform = np.array([
-            [0.00, target_g, target_r],
-            [0.00, -target_r, target_g],
-            [1.00, 0.00, 0.00],
+            A1,
+            A2,
+            np.cross(A1, A2),
         ])
         frame_dot = frame @ transform
         # look for cases where other basis vectors are low
+
         frame_dot[frame_dot < 0] = 0
-        high_intensity = frame_dot[..., 0] > ((frame_dot[..., 1] + frame_dot[..., 2]) * 4)
+        ratio = frame_dot[..., 0] / ((abs(frame_dot[..., 1]) + abs(frame_dot[..., 2])) / 2 + 1)
+        cv2.imshow('ratio', np.minimum(100 * ratio, 255).astype(np.uint8))
 
+        high_orangeness = frame_dot[..., 0] > ((frame_dot[..., 1] + frame_dot[..., 2]) * 2)
+        dark = frame.sum(axis=-1) < 100
 
-        frame_dot[~high_intensity] = 0
+        cv2.imshow('frame_dot_unfiltered', frame_dot.astype(np.uint8))
+        frame_dot[~high_orangeness | dark] = 0
         cv2.imshow('frame_dot', frame_dot.astype(np.uint8))
-
-        # frame_dot = (frame * np.array([0.00, 0.34, 0.66])).sum(axis=-1)
-        # frame_dot = frame_dot.astype(np.uint8)
-
-        # cv2.imshow('frame_dot', frame_dot.astype(np.uint8))
+        cv2.imshow('dark', dark.astype(np.uint8) * 255)
 
         ball_mask = (frame_dot > 0).any(axis=-1)
         height = ball_mask.shape[0]
@@ -77,13 +91,13 @@ try:
         # Erode and dilate
         erode_size = 3
         # kernel = np.ones((erode_size, erode_size), np.uint8)
-        kernel = np.array([
-            [0, 1, 0],
-            [1, 1, 1],
-            [0, 1, 0],
-        ], np.uint8)
-        ball_mask_u8 = cv2.erode(ball_mask_u8, kernel, iterations=1)
-        ball_mask_u8 = cv2.dilate(ball_mask_u8, kernel, iterations=1)
+        # kernel = np.array([
+        #     [0, 1, 0],
+        #     [1, 1, 1],
+        #     [0, 1, 0],
+        # ], np.uint8)
+        # ball_mask_u8 = cv2.erode(ball_mask_u8, kernel, iterations=1)
+        # ball_mask_u8 = cv2.dilate(ball_mask_u8, kernel, iterations=1)
 
         history.append(ball_mask_u8)
 
@@ -99,6 +113,12 @@ try:
                 continue
             cX = int(M["m10"] / M["m00"])
             cY = int(M["m01"] / M["m00"])
+
+            is_near_previous_detection = False
+            if previous_detection is not None:
+                is_near_previous_detection = np.linalg.norm(np.array([cX, cY]) - np.array(previous_detection)) < 50
+
+            # https://docs.opencv.org/4.x/d1/d32/tutorial_py_contour_properties.html
 
             # get area of contour
             area = cv2.contourArea(contour)
@@ -119,10 +139,17 @@ try:
             above_penalty = 1.0
             area_penalty = (min_expected_area - min(area, min_expected_area)) * below_penalty + (max(area, max_expected_area) - max_expected_area) * above_penalty
 
-            if area < 10:
-                continue
+            # kill tail end detections
+            if not is_near_previous_detection:
+                if area < 10:
+                    continue
+                if area > 500:
+                    continue
+                if eccentricity > 5:
+                    continue
 
             score = (solidity * 0.2 - eccentricity) - area_penalty * 0.1
+            score = -area_penalty * 0.1
             contours_with_scores.append((score, contour))
 
         contours_with_scores.sort(key=lambda x: x[0], reverse=True)
@@ -130,6 +157,17 @@ try:
             score, contour = contours_with_scores[0]
             # draw the contour and center of the shape on the image
             cv2.drawContours(frame, [contour], -1, (255, 0, 0), -1)
+
+            # draw a bigass circle
+            M = cv2.moments(contour)
+            if M["m00"] == 0:
+                continue
+            cX = int(M["m10"] / M["m00"])
+            cY = int(M["m01"] / M["m00"])
+            cv2.circle(frame, (cX, cY), 20, (0, 255, 255), 5)
+            previous_detection = (cX, cY)
+        else:
+            previous_detection = None
 
         cv2.imshow('frame', frame)
         # cv2.imshow('ball_mask', ball_mask)
